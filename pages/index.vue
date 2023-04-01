9f1d8e2415ad7700991a9691520a268dc7681a0f
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-04-01 22:58:27 +0430

    prettier format

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="w-1/2 mx-auto mt-16">
         <ClientOnly>
-            <Form
-                    :initial-values="{dateOfBirth:''}"
+            <Form :initial-values="{dateOfBirth:''}"
                     :validation-schema="schema"
                     @submit="sendData">
                 <base-date-picker simple class="mt-" name="dateOfBirth" label="date of birth" max="2008/05/02"/>
-                <button data-color="blue" type="submit" class="bg-green-400 border border-blue-400 rounded-lg px-6 py-2 mt-12"> Submit</button>
+                <button data-color="blue" type="submit"
+                        class="bg-green-400 border border-blue-400 rounded-lg px-6 py-2 mt-12"> Submit
+                </button>
             </Form>
         </ClientOnly>
     </div>
@@ -16,15 +17,9 @@ import {Form} from 'vee-validate'
 import {useSideValidator} from '~/composables/schemaValidate'
 
 const {schema} = useSideValidator()
-const maxDate = ref("")
-const {$moment} = useNuxtApp()
-function calculateMaxDate(){
-    maxDate.value = $moment()
-}
+
 function sendData(data: any) {
     console.log(data)
 }
-onMounted(()=>{
-    calculateMaxDate()
-})
+
 </script>
